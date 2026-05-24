637015ad1a21aac9ba4126625145cd5b226b95ee
TensorTeen <88822761+TensorTeen@users.noreply.github.com>
2026-05-24 17:02:17 +0000

    Deploying to gh-pages from @ TensorTeen/tensorteen.github.io@072023f8955505a1b0350dde998f06309742c45d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,8 +405,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-page-is-under-construction",
+          title: 'Page is under construction !',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
