4b00b3845952d0ba7a630e6f1ae3bad248170645
TensorTeen <88822761+TensorTeen@users.noreply.github.com>
2026-05-29 16:35:49 +0000

    Deploying to gh-pages from @ TensorTeen/tensorteen.github.io@63240c350f8c1ea56b7a3cae94e2170cbc0232a7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -476,9 +476,9 @@ ninja.data = [{
           description: "Bringing Geo-Spatial Context to LLMs at the Bharatiya Antariksh Hackathon 2024",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-project-optiwing",
+          title: 'Project Optiwing',
+          description: "Enforcing geometric smoothness in deep learning-based inverse airfoil design using spatial gradients.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
