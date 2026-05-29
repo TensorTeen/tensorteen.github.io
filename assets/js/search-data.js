3e42513b8c8599f9df036733ae59c3d120fc5570
TensorTeen <88822761+TensorTeen@users.noreply.github.com>
2026-05-29 10:30:25 +0000

    Deploying to gh-pages from @ TensorTeen/tensorteen.github.io@1cb64a6aa17d9f4ad9d3cbe1eba02b6407d4c234 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,18 +405,18 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-i-have-joined-adobe-media-and-data-science-research-lab-as-an-intern-working-on-building-multi-agent-collaborative-rl-framework-for-video-tasks",
-          title: 'I have joined Adobe Media and Data Science Research Lab as an intern...',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "news-i-have-joined-sarvam-ai-as-an-intern-working-on-building-ai-agents-for-multi-lingual-healthcare-use-case",
           title: 'I have joined Sarvam.AI as an intern working on building AI agents for...',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "news-i-have-joined-adobe-media-and-data-science-research-lab-as-an-intern-working-on-building-multi-agent-collaborative-rl-framework-for-video-tasks",
+          title: 'I have joined Adobe Media and Data Science Research Lab as an intern...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
