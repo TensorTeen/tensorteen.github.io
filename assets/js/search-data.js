c71a38d812d8025cadf728cc2a70f130a3a1637b
TensorTeen <88822761+TensorTeen@users.noreply.github.com>
2026-05-29 11:00:06 +0000

    Deploying to gh-pages from @ TensorTeen/tensorteen.github.io@c1764c913b736094c40a80819e648a0cb9e2717d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -408,7 +408,12 @@ ninja.data = [{
             },},{id: "news-i-have-joined-sarvam-ai-as-an-intern-working-on-building-ai-agents-for-multi-lingual-healthcare-use-case",
           title: 'I have joined Sarvam.AI as an intern working on building AI agents for...',
           description: "",
-          section: "News",},{id: "news-i-ve-been-selected-for-the-kiran-award-2024-the-best-academic-student-by-the-indian-society-for-non-destructive-testing-isnt",
+          section: "News",},{id: "news-our-work-on-finance-ai-has-been-accepted-in-finnlp-emnlp-2025-we-present-quot-enhancing-financial-rag-with-agentic-ai-and-multi-hyde-quot",
+          title: 'Our work on Finance AI has been accepted in FinNLP@EMNLP 2025, we present...',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_8/";
+            },},{id: "news-i-ve-been-selected-for-the-kiran-award-2024-the-best-academic-student-by-the-indian-society-for-non-destructive-testing-isnt",
           title: 'I’ve been selected for the Kiran Award 2024 – “The Best Academic Student”...',
           description: "",
           section: "News",handler: () => {
