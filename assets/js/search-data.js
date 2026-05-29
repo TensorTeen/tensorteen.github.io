414bd02546acead95020892ba67717f7fdb6e86e
TensorTeen <88822761+TensorTeen@users.noreply.github.com>
2026-05-29 10:55:57 +0000

    Deploying to gh-pages from @ TensorTeen/tensorteen.github.io@82bfe044d4c684acd69ebe57b5aca996dea21014 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -435,7 +435,7 @@ ninja.data = [{
           title: '',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcements_7/";
+              window.location.href = "/news/announcement_7/";
             },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
