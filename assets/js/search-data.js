7c57321c84fa4a00dd12a06a876e671f265874ea
TensorTeen <88822761+TensorTeen@users.noreply.github.com>
2026-05-29 12:03:49 +0000

    Deploying to gh-pages from @ TensorTeen/tensorteen.github.io@c8dbdfc824a8fff3d27b9bf9e0faf7d0292fb09c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -461,9 +461,9 @@ ninja.data = [{
           description: "Accelerating Stiff PDE Resolution via Kernel-Adaptive Physics-Informed Extreme Learning Machines (PI-ELMs)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-beyond-the-moment-conditioning-frozen-vlas-on-memory-for-long-horizon-manipulation-tasks",
+          title: 'Beyond the Moment, Conditioning Frozen VLAs on Memory for Long-Horizon Manipulation Tasks',
+          description: "Developed a **Training-Free Memory Conditioned Action Generation** framework. This non-parametric, retrieval-augmented system conditions a frozen VLA on historical expert trajectories, guiding action generation without requiring any fine-tuning or architectural changes.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
