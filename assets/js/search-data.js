ca145ff4d3b6b24e766ab2080ce548894918443e
TensorTeen <88822761+TensorTeen@users.noreply.github.com>
2026-05-30 14:14:18 +0000

    Deploying to gh-pages from @ TensorTeen/tensorteen.github.io@70f49341a181dce1cc09637744fb634f1ba4e750 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -477,11 +477,6 @@ ninja.data = [{
           description: "A Dataset and Bias Evaluation Framework in LLMs Using Contrastive Embedding Similarity in the Indian Context",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
